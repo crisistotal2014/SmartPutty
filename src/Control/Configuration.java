eb87f12d206190e5430da6ee22cf4b8cf1290330
Carlos SS <crisistotal2014@gmail.com>
2016-06-24 14:52:03 +0200

    Add files via upload
    Use "Configuration.xml" to store more config settings, like toolbars to view. Also re-enabled "saveConfiguration()"
    when programs exits to store changed values (related to visible toolbars).

--- src/Control/Configuration.java
+++ src/Control/Configuration.java
@@ -1,82 +1,112 @@
 package Control;
 
+import UI.MainFrame;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
 public class Configuration {
+	// XML configuration file to use:
+	private static final String CONFIG_FILE = "Configuration.xml";
 
-	private static Configuration conf = null;
-	private Properties prop = new Properties();
-	private Configuration() {
+	private final Properties prop;
+	private final MainFrame mainFrame;
+
+	// Constructor:
+	public Configuration(MainFrame mainFrame){
+		this.mainFrame = mainFrame;
+
+		prop = new Properties();
 		loadConfiguration();
 	}
 
-	public static Configuration getInstance() {
-		if (conf == null)
-			conf = new Configuration();
-		return conf;
-	}
-
-//	private void saveConfiguration() {
-//		try {
-//			Properties prop = new Properties();
-//			prop.setProperty("Timeout", "10000");
-//			prop.setProperty("WaitForInitTime", "1500");
-//			prop.setProperty("ProxyHost", "tipl01.swg.usma.ibm.com");
-//			prop.setProperty("ProxyUser", "perfadmin");
-//			prop.setProperty("ProxyPassword", "tipadmin");
-//			prop.setProperty("ProxyPort", "7890");
-//			FileOutputStream fos = new FileOutputStream("Configuration.xml");
-//			prop.storeToXML(fos,"configuration file");
-//			fos.close();
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-
-	private void loadConfiguration() {
-		FileInputStream fis = null;
+	/**
+	 * Save program configuration.
+	 */
+	public void saveConfiguration(){
 		try {
-			fis = new FileInputStream("Configuration.xml");
-			prop.loadFromXML(fis);
-		} catch (InvalidPropertiesFormatException e) {
+			FileOutputStream fos = new FileOutputStream(CONFIG_FILE);
+
+			// Proxy settings:
+			prop.setProperty("Timeout", "10000");
+			prop.setProperty("WaitForInitTime", "1500");
+			prop.setProperty("ProxyHost", "tipl01.swg.usma.ibm.com");
+			prop.setProperty("ProxyUser", "perfadmin");
+			prop.setProperty("ProxyPassword", "tipadmin");
+			prop.setProperty("ProxyPort", "7890");
+			// Main window view toolbars:
+			prop.setProperty("ViewUtilitiesBar", String.valueOf(mainFrame.utilitiesBarMenuItem.getSelection()));
+			prop.setProperty("ViewConnectionBar", String.valueOf(mainFrame.connectionBarMenuItem.getSelection()));
+
+			prop.storeToXML(fos, "SmartPutty configuration file");
+			fos.close();
+		} catch (FileNotFoundException e){
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (IOException e){
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		prop.list(System.out);
 	}
 
-	public String getTimeout() {
+	/**
+	 * Load program configuration.
+	 */
+	private void loadConfiguration(){
+		try {
+			FileInputStream fis = new FileInputStream(CONFIG_FILE);
+			prop.loadFromXML(fis);
+		} catch (InvalidPropertiesFormatException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		prop.list(System.out); //DEBUG
+	}
+
+	public String getTimeout(){
 		return (String) prop.get("Timeout");
 	}
 
-	public String getWaitForInitTime() {
+	public String getWaitForInitTime(){
 		return (String) prop.get("WaitForInitTime");
 	}
 
-	public String getProxyHost() {
+	public String getProxyHost(){
 		return (String) prop.get("ProxyHost");
 	}
 
-	public String getProxyUser() {
+	public String getProxyUser(){
 		return (String) prop.get("ProxyUser");
 	}
 
-	public String getProxyPort() {
+	public String getProxyPort(){
 		return (String) prop.get("ProxyPort");
 	}
 
-	public String getProxyPassword() {
+	public String getProxyPassword(){
 		return (String) prop.get("ProxyPassword");
 	}
 
+	/**
+	 * Utilities bar must be visible?
+	 * @return 
+	 */
+	public Boolean getUtilitiesVisible(){
+		return Boolean.valueOf((String) prop.get("ViewUtilitiesBar"));
+	}
+
+	/**
+	 * Connection bar must be visible?
+	 * @return 
+	 */
+	public Boolean getConnectionVisible(){
+		return Boolean.valueOf((String) prop.get("ViewConnectionBar"));
+	}
 }
